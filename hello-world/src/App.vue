9cf2cd26bac08023ad637b7b4c1064fbdb6170d9
Ralph Lee <ralph.lee@live.ca>
2021-05-19 16:10:54 +0900

    Ternary operator with v-bind

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -5,6 +5,7 @@
   <h2 class="underline">Underlined Text</h2>
   <h2 v-bind:class="status">Status</h2>
   <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout</h2>
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
@@ -22,6 +23,7 @@ export default {
       isDisabled: true,
       status: "success",
       isPromoted: true,
+      isSoldOut: true,
     };
   },
 };
@@ -43,5 +45,13 @@ export default {
 
 .promoted {
   font-style: italic;
+}
+
+.new {
+  color: green;
+}
+
+.sold-out {
+  color: red;
 }
 </style>

8efc96dd511f517067efa2a5a6608ee1e8d26f0c
Ralph Lee <ralph.lee@live.ca>
2021-05-25 23:14:39 +0900

    Apply v-else conditional

--- hello-world/src/App2.vue
+++ hello-world/src/App2.vue
@@ -1,5 +1,6 @@
 <template>
   <h2 v-if="num === 0">The number is zero</h2>
+  <h2 v-else>The number is NOT zero</h2>
 </template>
 
 <script>
@@ -7,7 +8,7 @@ export default {
   name: "App2",
   data() {
     return {
-      num: 0,
+      num: 1,
     };
   },
 };

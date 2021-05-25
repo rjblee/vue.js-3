54316e52411520c548a1793839d98ca14208360b
Ralph Lee <ralph.lee@live.ca>
2021-05-25 23:05:05 +0900

    Apply v-if conditional rendering

---
+++ hello-world/src/App2.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2 v-if="num === 0">The number is zero</h2>
+</template>
+
+<script>
+export default {
+  name: "App2",
+  data() {
+    return {
+      num: 0,
+    };
+  },
+};
+</script>
+
+<style></style>

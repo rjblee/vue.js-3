2ff86e780ac0b04d028ebd66507755ae535d1a62
Ralph Lee <ralph.lee@live.ca>
2021-05-26 23:52:33 +0900

    Apply v-else-if conditional

--- hello-world/src/App2.vue
+++ hello-world/src/App2.vue
@@ -1,6 +1,8 @@
 <template>
   <h2 v-if="num === 0">The number is zero</h2>
-  <h2 v-else>The number is NOT zero</h2>
+  <h2 v-else-if="num < 0">The number is negative</h2>
+  <h2 v-else-if="num > 0">The number is positive</h2>
+  <h2 v-else>Not a number</h2>
 </template>
 
 <script>

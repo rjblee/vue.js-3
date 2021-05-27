347fa6f9c3c092aeb2b3ef0a6bd8d6c408e38c38
Ralph Lee <ralph.lee@live.ca>
2021-05-27 22:13:51 +0900

    V-for with objects in an array

--- hello-world/src/App3.vue
+++ hello-world/src/App3.vue
@@ -1,5 +1,8 @@
 <template>
   <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+  <h2 v-for="name in fullNames" :key="name.first">
+    {{ name.first }} {{ name.last }}
+  </h2>
 </template>
 
 <script>
@@ -7,6 +10,11 @@ export default {
   data() {
     return {
       names: ["Ralph", "Clark", "Michelle"],
+      fullNames: [
+        { first: "Ralph", last: "Lee" },
+        { first: "Clark", last: "Yoon" },
+        { first: "Michelle", last: "Kim" },
+      ],
     };
   },
 };

9f0648967b0e62980e294c31d91446e0ceb23057
Ralph Lee <ralph.lee@live.ca>
2021-05-27 20:58:24 +0900

    V-show and v-if

--- hello-world/src/App2.vue
+++ hello-world/src/App2.vue
@@ -3,6 +3,15 @@
   <h2 v-else-if="num < 0">The number is negative</h2>
   <h2 v-else-if="num > 0">The number is positive</h2>
   <h2 v-else>Not a number</h2>
+
+  <div v-if="display">
+    <h2>Ralph</h2>
+    <h2>Ralph2</h2>
+    <h2>Ralph3</h2>
+  </div>
+
+  <h2 v-show="showElement">Using v-show</h2>
+  <h2 v-if="showElement">Using v-if</h2>
 </template>
 
 <script>
@@ -10,7 +19,9 @@ export default {
   name: "App2",
   data() {
     return {
-      num: 1,
+      num: "Hi",
+      display: true,
+      showElement: true,
     };
   },
 };

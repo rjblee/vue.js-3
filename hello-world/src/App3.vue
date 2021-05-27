a03edfe4720b74ac151c5fe3f3109cefb263b79c
Ralph Lee <ralph.lee@live.ca>
2021-05-27 21:59:49 +0900

    Bind an array of anmes with v-for

---
+++ hello-world/src/App3.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      names: ["Ralph", "Clark", "Michelle"],
+    };
+  },
+};
+</script>
+
+<style></style>

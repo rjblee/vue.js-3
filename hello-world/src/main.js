47bfd5f49213576334a2896100e4433bf18c7ef4
Ralph Lee <ralph.lee@live.ca>
2021-05-29 04:11:26 +0900

    Import App3 file

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
 // import App from "./App.vue";
-import App2 from "./App2.vue";
+import App4 from "./App4.vue";
 
-createApp(App2).mount("#app");
+createApp(App4).mount("#app");

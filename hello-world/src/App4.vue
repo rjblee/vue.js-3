9bdc068ce9e06359e579db0c3f103409578c42d7
Ralph Lee <ralph.lee@live.ca>
2021-05-28 14:39:42 +0900

    Learn about methods

---
+++ hello-world/src/App4.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>{{ 2 + 3 + 5 }}</h2>
+  <h2>Add method: {{ add(2, 3, 5) }}</h2>
+  <h2>Add method: {{ add(10, 15, 20) }}</h2>
+  <h2>Add method: {{ multiply(baseValue) }}</h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      baseMultiplier: 5,
+      baseValue: 2,
+    };
+  },
+  methods: {
+    add(a, b, c) {
+      return a + b + c;
+    },
+    multiply(num) {
+      return num * this.baseMultiplier;
+    },
+  },
+};
+</script>
+
+<style></style>

c8d622ecfa85d28a0431f021ef9d5f9e7a49ee7d
Ralph Lee <ralph.lee@live.ca>
2021-05-30 23:57:52 +0900

    Apply v-model to bind input data

---
+++ hello-world/src/App5.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <label for="profile">Profile Summary</label>
+      <textarea
+        name=""
+        id="profile"
+        v-model="formValues.profileSummary"
+        cols="30"
+        rows="10"
+      ></textarea>
+    </div>
+
+    <div>
+      <label for="country">Country</label>
+      <select name="" id="country">
+        <option value="">Select a Country</option>
+        <option value="canada">Canada</option>
+        <option value="korea">Korea</option>
+        <option value="usa">USA</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

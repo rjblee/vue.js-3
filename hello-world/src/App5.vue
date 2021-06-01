00d008fe9cedd6115d46c0202b18d0d3952cccd5
Ralph Lee <ralph.lee@live.ca>
2021-06-01 17:20:16 +0900

    Add multiselect checkboxes, multiple input selection, boolean checkbox

--- hello-world/src/App5.vue
+++ hello-world/src/App5.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
+    <pre
+      >{{ JSON.stringify(formValues, null, 2) }}
+    </pre>
+  </div>
+  <form @submit="submitForm">
     <div>
-      <label for="profile">Profile Summary</label>
+      <label for="name">Name</label><br />
+      <input type="text" id="name" v-model="formValues.name" />
+    </div>
+    <br />
+    <div>
+      <label for="profile">Profile Summary</label><br />
       <textarea
         name=""
         id="profile"
@@ -10,27 +20,69 @@
         rows="10"
       ></textarea>
     </div>
-
+    <br />
     <div>
-      <label for="country">Country</label>
-      <select name="" id="country">
+      <label for="country">Country</label><br />
+      <select id="country" v-model="formValues.country">
         <option value="">Select a Country</option>
         <option value="canada">Canada</option>
         <option value="korea">Korea</option>
+        <option value="korea">Australia</option>
         <option value="usa">USA</option>
       </select>
     </div>
-
+    <br />
     <div>
-      <input
-        type="checkbox"
-        name=""
-        id="remoteWork"
-        v-model="formValues.remoteWork"
-      />
+      <label for="job-location">Job Location</label><br />
+      <select multiple id="job-location" v-model="formValues.jobLocation">
+        <option value="canada">Canada</option>
+        <option value="korea">Korea</option>
+        <option value="australia">Australia</option>
+        <option value="usa">USA</option>
+      </select>
+    </div>
+    <br />
+    <div>
+      <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" />
       <label for="remoteWork">Open to remote work?</label>
     </div>
-  </div>
+    <br />
+    <div>
+      <label for="remoteWork">Skill Set</label><br />
+      <input
+        type="checkbox"
+        id="html"
+        value="html"
+        v-model="formValues.skillSet"
+      />
+      <label for="html">HTML</label>
+      <input
+        type="checkbox"
+        id="css"
+        value="css"
+        v-model="formValues.skillSet"
+      />
+      <label for="css">CSS</label>
+      <input
+        type="checkbox"
+        id="javascript"
+        value="javascript"
+        v-model="formValues.skillSet"
+      />
+      <label for="javascript">JavaScript</label>
+      <input
+        type="checkbox"
+        id="vue"
+        value="vue"
+        v-model="formValues.skillSet"
+      />
+      <label for="vue">Vue</label>
+    </div>
+
+    <div>
+      <button>Submit</button>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -43,11 +95,24 @@ export default {
         country: "",
         jobLocation: [],
         remoteWork: false,
+        skillSet: [],
+        yearsOfExperience: "",
       },
     };
   },
-  methods: {},
+  methods: {
+    submitForm() {
+      event.preventDefault();
+      console.log("Form Values", this.formValues);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>

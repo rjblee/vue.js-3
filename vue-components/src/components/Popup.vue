cf013c2ff3d9c84de208ee827437d4243aaf0b78
Ralph Lee <ralph.lee@live.ca>
2021-06-09 15:40:42 +0900

    Create a Popup component

---
+++ vue-components/src/components/Popup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>This is a popup</h2>
+    <button @click="$emit('close')">Close</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Popup",
+  emits: ["close"],
+};
+</script>
+
+<style lang="scss" scoped></style>

4974426d60fce86ec703d6787bba5d7a17b189f3
Ralph Lee <ralph.lee@live.ca>
2021-06-11 23:08:42 +0900

    Create a names array of data with NameList component

---
+++ vue-components/src/components/NameList.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 v-for="name in names" :key="name.firstName">
+    {{ name.firstName }} {{ name.lastName }}
+  </h2>
+</template>
+
+<script>
+export default {
+  name: "NameList",
+  data() {
+    return {
+      names: [
+        { firstName: "Ralph", lastName: "Lee" },
+        { firstName: "Tommy", lastName: "Kim" },
+        { firstName: "Andy", lastName: "Seo" },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

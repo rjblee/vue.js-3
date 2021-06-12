aa5e4037e62b37cd93d12bbff525ee7d2d118a11
Ralph Lee <ralph.lee@live.ca>
2021-06-12 23:11:30 +0900

    Create ChildStyles component with scoped css

---
+++ vue-components/src/components/ChildStyles.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h4>ChildStyles text</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChildStyles",
+};
+</script>
+
+<style scoped>
+h4 {
+  color: red;
+}
+</style>

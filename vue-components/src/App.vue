1c15e9cc3927ae4cf62fc01dfa6457540ab8aa6f
Ralph Lee <ralph.lee@live.ca>
2021-06-14 02:25:08 +0900

    Wrap keep-alive tag around component tag to keep data

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -3,7 +3,10 @@
   <button @click="activeTab = 'TabB'">Tab B</button>
   <button @click="activeTab = 'TabC'">Tab C</button>
 
-  <component :is="activeTab" />
+  <keep-alive>
+    <component :is="activeTab" />
+  </keep-alive>
+
   <!-- <TabA v-if="activeTab === 'TabA'" />
   <TabB v-if="activeTab === 'TabB'" />
   <TabC v-if="activeTab === 'TabC'" /> -->

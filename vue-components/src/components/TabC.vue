06a100f644569f9dd05e3c8462fc8e672fafc0a9
Ralph Lee <ralph.lee@live.ca>
2021-06-14 02:24:42 +0900

    Insert an input with name data

--- vue-components/src/components/TabC.vue
+++ vue-components/src/components/TabC.vue
@@ -2,11 +2,17 @@
   <div>
     Tab C Content
   </div>
+  <input type="text" v-model="name" />
 </template>
 
 <script>
 export default {
   name: "TabC",
+  data() {
+    return {
+      name: "",
+    };
+  },
 };
 </script>
 

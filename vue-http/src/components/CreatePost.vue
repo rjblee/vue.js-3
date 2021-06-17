7ef44579c24e6f43a70460160ba3570fc6dded87
Ralph Lee <ralph.lee@live.ca>
2021-06-17 22:52:47 +0900

    Add a post request

---
+++ vue-http/src/components/CreatePost.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <form action="">
+      <label for="userId">Post Id</label>
+      <input type="text" id="userId" />
+
+      <label for="title">Post Title</label>
+      <input type="text" id="title" />
+
+      <label for="body">Post Body</label>
+      <input type="text" id="body" />
+
+      <button>Sumit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: CreatePost,
+  data() {
+     return {
+        formData {
+           userId: '',
+           title: '',
+           body: '',
+
+        },
+     }
+  }
+};
+</script>
+
+<style scoped></style>
